e933f46e7a0f9343a12ec36fef722893d182462b
Jonathan <jtellkampf@gmail.com>
2021-05-23 05:21:58 +0100

    Completed styling movie card

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,4 +1,4 @@
-import { url } from "inspector";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { MovieGenreConfig } from "../../redux/reducers/config-reducer";
 import { MovieDetails } from "../../redux/reducers/movies-reducer";
@@ -17,32 +17,51 @@ const MovieCard: React.FC<MovieCardProps> = ({
   posterSize,
   movieGenres
 }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
     <Link
-      className="movie-card"
+      className=""
       to={`/details/${movie.name ? "tv" : "movie"}/${movie.id}`}
     >
       <div
-        className="movie-card-background"
-        style={{
-          background: `url(${secureBaseUrl}${posterSize}${movie.poster_path})  no-repeat`
-        }}
+        className="movie-card"
+        onMouseEnter={() => setShowDetails(true)}
+        onMouseLeave={() => setShowDetails(false)}
       >
-        <div className="movie-card-body">
-          {/* <h2 className="movie-card-title">{movie.title}</h2> */}
-          <div className="movie-card-rating">
-            <i className="fas fa-star movie-card-rating-icon"></i>
-            <div className="movie-card-rating-count">
-              {movie.vote_average.toFixed(1)}
+        <div
+          className="movie-card-background"
+          style={{
+            background: `url(${secureBaseUrl}${posterSize}${movie.poster_path})  no-repeat`
+          }}
+        >
+          <div className="movie-card-body">
+            <div className="movie-card-rating">
+              <i className="fas fa-star movie-card-rating-icon"></i>
+              <div className="movie-card-rating-count">
+                {movie.vote_average.toFixed(1)}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="movie-card-details">
-        {movie.genre_ids.map(genreId => {
-          const genre = movieGenres.genres.find(genre => genre.id === genreId);
-          return <span key={genreId}>{genre?.name} </span>;
-        })}
+        {showDetails && (
+          <div className="movie-card-details">
+            <h2 className="movie-card-details-title">
+              {movie.title} &#40;{new Date(movie.release_date).getFullYear()}
+              &#41;
+            </h2>
+            {movie.genre_ids.map(genreId => {
+              const genre = movieGenres.genres.find(
+                genre => genre.id === genreId
+              );
+              return (
+                <span className="movie-card-details-genres" key={genreId}>
+                  {genre?.name} &#183;{" "}
+                </span>
+              );
+            })}
+          </div>
+        )}
       </div>
     </Link>
   );
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { useActions } from "../../hooks/useActions";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 import Header from "../Header";
 import MovieCard from "../MovieCard";
+
+import "./profile.scss";
 
 const Profile: React.FC = () => {
   const [state, setState] = useState<null | string>("");
@@ -15,16 +17,19 @@ const Profile: React.FC = () => {
   const config = useTypedSelector(state => state.config);
 
   return (
-    <div>
-      {movies.results.map(movie => (
-        <MovieCard
-          movieGenres={config.movieGenres}
-          secureBaseUrl={config.images.images.secure_base_url}
-          posterSize={config.images.images.poster_sizes[2]}
-          movie={movie}
-        />
-      ))}
-    </div>
+    <Fragment>
+      <Header />
+      <div className="profile">
+        {movies.results.map(movie => (
+          <MovieCard
+            movieGenres={config.movieGenres}
+            secureBaseUrl={config.images.images.secure_base_url}
+            posterSize={config.images.images.poster_sizes[2]}
+            movie={movie}
+          />
+        ))}
+      </div>
+    </Fragment>
   );
 };
 
